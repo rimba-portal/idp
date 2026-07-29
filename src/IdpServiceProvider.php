1ed2a0619454db49a6af1aff1c11bbba59550cf9
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-29 15:51:18 +0800

    Release v1.0.1

---
+++ src/IdpServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rimba\Idp;
+
+use Rimba\Base\Services\BitesServiceProvider;
+
+class IdpServiceProvider extends BitesServiceProvider
+{
+    protected function bootPackage(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        //
+    }
+
+    protected function registerPackage(): void
+    {
+        //
+    }
+}
