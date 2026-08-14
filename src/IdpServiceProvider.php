035b5ff65f2ebad979be9c13ce3a0d7f88ea2ec3
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-14 15:17:14 +0800

    Release v1.1.1

--- src/IdpServiceProvider.php
+++ src/IdpServiceProvider.php
@@ -6,7 +6,6 @@ namespace Rimba\Idp;
 
 use Illuminate\Routing\Router;
 use Rimba\Base\Services\BitesServiceProvider;
-use Rimba\Idp\Console\Commands\DiagnoseIdp;
 use Rimba\Idp\Contracts\UserInfoResolverContract;
 use Rimba\Idp\Http\Middleware\EnsureClientApiAccess;
 use Rimba\Idp\Services\PassportConfigurator;
@@ -14,20 +13,24 @@ use Rimba\Idp\Services\UserInfoService;
 
 class IdpServiceProvider extends BitesServiceProvider
 {
-    protected string $configPath = __DIR__.'/../config/idp.php';
+    protected string $configFile = __DIR__.'/../config/bites.php';
 
     protected string $viewsPath = __DIR__.'/../resources/views';
 
-    protected array $commands = [DiagnoseIdp::class];
-
-    protected function registerPackage(): void
-    {
-        $this->app->bind(UserInfoResolverContract::class, UserInfoService::class);
-    }
+    protected string $iconsPath = __DIR__.'/../resources/svg';
 
     protected function bootPackage(): void
     {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         app(PassportConfigurator::class)->boot();
         $this->app->make(Router::class)->aliasMiddleware('idp.client-api', EnsureClientApiAccess::class);
+
+    }
+
+    protected function registerPackage(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/bites_idp.php', 'bites_idp');
+        $this->app->bind(UserInfoResolverContract::class, UserInfoService::class);
+
     }
 }
